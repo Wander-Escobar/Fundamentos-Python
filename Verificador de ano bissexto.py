257e5d3e4ae81877c7c3efd53dcd2a6845116cba
Wanderlan Escobar Gonzaga <wanderlan.gonzaga@gmail.com>
2025-06-17 16:57:18 -0300

    Add files via upload

---
+++ Verificador de ano bissexto.py
@@ -0,0 +1,8 @@
+print('Verificador de ano bissexto\n')
+
+ano = int(input('Informe um ano: '))
+
+if (ano % 4 == 0) and ((ano % 100 != 0) or (ano % 400 == 0)):
+    print(f'\nO ano {ano} é bissexto! ')
+else:
+    print(f'\nO ano {ano} NÃO é bissexto! ')
