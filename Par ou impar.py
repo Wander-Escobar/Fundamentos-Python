2119e8c68794c218259c38741e0f2a69df624703
Wanderlan Escobar Gonzaga <wanderlan.gonzaga@gmail.com>
2025-06-17 12:55:44 -0300

    Add files via upload

---
+++ Par ou impar.py
@@ -0,0 +1,8 @@
+print('Par ou impar\n')
+
+numero = int(input('Vamos verificar se o número é par ou ímpar \nDigite um numero: '))
+
+if numero%2 == 0:
+    print(f'O número {numero} é PAR! ')
+else:
+    print(f'O número {numero} é IMPAR! ')
